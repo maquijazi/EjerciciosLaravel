c6ae2df6ad447fa61d6edb31a96ea4678ae3cda3
maquijazi <xavi90_8@hotmail.com>
2020-10-19 19:33:21 +0200

    con bootstrap

--- EjemploQuiz/resources/views/layouts/ejemplolayout.blade.php
+++ EjemploQuiz/resources/views/layouts/ejemplolayout.blade.php
@@ -2,6 +2,8 @@
 
 <head>
     <title>Nombre de la app - @yield('titulo')</title>
+    <script src="{{ asset('js/app.js')}}" defer></script>
+    <link href="{{ asset('css/app.css')}}" rel="stylesheet">
 </head>
 
 <body>
